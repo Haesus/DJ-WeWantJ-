1eca6df94216013c3754b0b3f8963029a08b986e
Tag <sea15510@gmail.com>
2024-06-02 15:16:55 +0900

    [DOCS] XcodeProject 파일 추가 및 Node 폴더 추가
    -

---
+++ DJ(DailyJournal)-Swift/DJ(DailyJournal)/DJ_DailyJournal_App.swift
@@ -0,0 +1,17 @@
+//
+//  DJ_DailyJournal_App.swift
+//  DJ(DailyJournal)
+//
+//  Created by 윤해수 on 6/2/24.
+//
+
+import SwiftUI
+
+@main
+struct DJ_DailyJournal_App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
