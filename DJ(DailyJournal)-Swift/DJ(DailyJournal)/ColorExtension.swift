4d7444b11d645d745157cfe3bd60a9a1005b62d9
Tag <sea15510@gmail.com>
2024-06-04 17:30:36 +0900

    [FEAT] Color Extension backgroundColor 추가
    backgroundColor용 변수 생성
     - 그라데이션 색 적용
    
    -

--- DJ(DailyJournal)-Swift/DJ(DailyJournal)/ColorExtension.swift
+++ DJ(DailyJournal)-Swift/DJ(DailyJournal)/ColorExtension.swift
@@ -11,18 +11,20 @@ extension Color {
     init(hex: String, alpha: Double = 1.0) {
         let hexFormatted = hex.trimmingCharacters(in: CharacterSet.alphanumerics.inverted)
         var int = UInt64()
-        Scanner(string: hexFormatted).scanHexInt64(&int)
         let a, r, g, b: UInt64
+        Scanner(string: hexFormatted).scanHexInt64(&int)
+        
         switch hexFormatted.count {
-        case 3: // RGB (12-bit)
-            (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
-        case 6: // RGB (24-bit)
-            (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
-        case 8: // ARGB (32-bit)
-            (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
-        default:
-            (a, r, g, b) = (255, 0, 0, 0)
+            case 3: // RGB (12-bit)
+                (a, r, g, b) = (255, (int >> 8) * 17, (int >> 4 & 0xF) * 17, (int & 0xF) * 17)
+            case 6: // RGB (24-bit)
+                (a, r, g, b) = (255, int >> 16, int >> 8 & 0xFF, int & 0xFF)
+            case 8: // ARGB (32-bit)
+                (a, r, g, b) = (int >> 24, int >> 16 & 0xFF, int >> 8 & 0xFF, int & 0xFF)
+            default:
+                (a, r, g, b) = (255, 0, 0, 0)
         }
+        
         self.init(
             .sRGB,
             red: Double(r) / 255,
@@ -32,6 +34,13 @@ extension Color {
         )
     }
     
+    static var backgroundColor: LinearGradient {
+        return LinearGradient(
+            gradient: Gradient(colors: [Color.topColor, Color.bottomColor]),
+            startPoint: .top,
+            endPoint: .bottom
+        )
+    }
     
     static let gray = Color(hex: "828282")
     static let lightYellow = Color(hex: "FFD27F")
