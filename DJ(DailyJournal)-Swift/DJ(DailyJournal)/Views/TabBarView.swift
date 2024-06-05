c43086da7899fc72ff3d4ea94cabcbf3d9aa903e
Tag <sea15510@gmail.com>
2024-06-05 17:13:14 +0900

    [REFACTOR] xcodedata 파일 수정
    -

--- DJ(DailyJournal)-Swift/DJ(DailyJournal)/Views/TabBarView.swift
+++ DJ(DailyJournal)-Swift/DJ(DailyJournal)/Views/TabBarView.swift
@@ -8,55 +8,32 @@
 import SwiftUI
 
 struct TabBarView: View {
-    @State private var selectedTab: Tab = .dailyjournal
-    
-    enum Tab {
-        case dailyjournal
-        case todolist
-        case dailyLog
-    }
-    
     var body: some View {
         NavigationStack {
-            TabView(selection: $selectedTab) {
+            TabView() {
                 // TODO: - JournalListView로 수정
                 TabBarView1()
                     .tabItem {
                         Image(systemName: "house")
                         Text("Home")
                     }
-                    .tag(Tab.dailyjournal)
                 
                 TodoListView()
                     .tabItem {
                         Image(systemName: "checkmark.seal")
                         Text("Todo")
                     }
-                    .tag(Tab.todolist)
               
                 DailyLogView()
                     .tabItem {
                         Image(systemName: "pencil.and.list.clipboard")
                         Text("DailyLog")
                     }
-                    .tag(Tab.dailyLog)
             }
             .tint(Color.lightYellow)
             .onAppear() {
                 UITabBar.appearance().unselectedItemTintColor = .gray
             }
-            .navigationTitle(navigationTitle(for: selectedTab))
-        }
-    }
-    
-    private func navigationTitle(for tab: Tab) -> String {
-        switch tab {
-            case .dailyjournal:
-                return "Daily Journal"
-            case .todolist:
-                return "Todo List"
-            case .dailyLog:
-                return "Daily Log"
         }
     }
 }
