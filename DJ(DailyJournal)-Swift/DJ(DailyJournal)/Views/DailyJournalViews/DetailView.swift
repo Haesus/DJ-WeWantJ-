dacc9036e32b4dd49871e2447d8c3ba9ba8e6370
Eujeen Han <eujeenhan@gmail.com>
2024-06-07 09:36:32 +0900

    Daily journal views 추가

---
+++ DJ(DailyJournal)-Swift/DJ(DailyJournal)/Views/DailyJournalViews/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  DJ(DailyJournal)
+//
+//  Created by 한유진 on 6/7/24.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
---
+++ DJ(DailyJournal)-Swift/DJ(DailyJournal)/Views/DailyJournalViews/ListView.swift
@@ -0,0 +1,18 @@
+//
+//  ListView.swift
+//  DJ(DailyJournal)
+//
+//  Created by 한유진 on 6/7/24.
+//
+
+import SwiftUI
+
+struct ListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListView()
+}
