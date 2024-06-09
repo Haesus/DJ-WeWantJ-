cf269ff2f7672ed4bfc9a984b279e4c820efd8f3
suye0ng <rlatndud456@naver.com>
2024-06-09 17:57:59 +0900

    [FIX] 테스트 주석 삭제

--- DJ(DailyJournal)-Swift/DJ(DailyJournal)/Views/DailyLogViews/DailyLogRowView.swift
+++ DJ(DailyJournal)-Swift/DJ(DailyJournal)/Views/DailyLogViews/DailyLogRowView.swift
@@ -1,61 +1,9 @@
-////
-////  DailyLogRowView.swift
-////  DJ(DailyJournal)
-////
-////  Created by 김수영 on 6/4/24.
-////
 //
-//import SwiftUI
+//  DailyLogRowView.swift
+//  DJ(DailyJournal)
 //
-//struct DailyLogRowView: View {
-//    @StateObject var dailyTemplateViewModel: TemplateViewModel<DailyTemplateModel>
-//    @Binding var dailyTemplate: DailyTemplateModel?
-//   
-//    var body: some View {
-//        VStack(alignment: .leading) {
-//            if let dailyLogList = dailyTemplate?.dailyLogList {
-//                List {
-//                    ForEach(dailyLogList.indices, id: \.self) { index in
-//                        HStack {
-//                            TextField("Title", text: Binding(
-//                                get: {
-//                                    dailyLogList[index].isDaily
-//                                },
-//                                set: { newValue in
-//                                    dailyTemplateViewModel.template?.dailyLogList[index].isDaily = newValue
-//                                }))
-//                            .foregroundColor(.ivory)
-//                            .font(.title2)
-//                          
-//                            Spacer()
-//                            
-//                            Text(" | ")
-//                            
-//                            TextField("내용 입력", text: Binding(
-//                                get: {
-//                                    dailyLogList[index].dailyText
-//                                },
-//                                set: { newValue in
-//                                    dailyTemplateViewModel.template?.dailyLogList[index].dailyText = newValue
-//                                }))
-//                            .foregroundColor(.ivory)
-//                            .font(.title2)
-//                            
-//                            Spacer()
-//                        }
-//                    }
-//                }
-//            }
-//        }
-//    }
-//}
+//  Created by 김수영 on 6/4/24.
 //
-//
-//
-//#Preview {
-//    DailyLogRowView(dailyTemplateViewModel: TemplateViewModel(), dailyTemplate: .constant(DailyTemplateModel(dailyLogList: [DailyLog(isDaily: "", dailyText: "")])))
-//}
-
 
 import SwiftUI
 
