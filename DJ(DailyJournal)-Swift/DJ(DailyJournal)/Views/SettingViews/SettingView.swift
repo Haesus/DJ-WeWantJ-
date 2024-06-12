10ff59a2ab17d1c6722023961a1b44f8b43ed80e
suye0ng <rlatndud456@naver.com>
2024-06-12 18:06:27 +0900

    [FEAT] 로그아웃 후에 SignInView 전환

--- DJ(DailyJournal)-Swift/DJ(DailyJournal)/Views/SettingViews/SettingView.swift
+++ DJ(DailyJournal)-Swift/DJ(DailyJournal)/Views/SettingViews/SettingView.swift
@@ -19,40 +19,53 @@ struct SafariView: UIViewControllerRepresentable {
 }
 
 struct SettingView: View {
-    @ObservedObject var signViewModel = SignViewModel()
+    @EnvironmentObject var signViewModel: SignViewModel
     @State private var showLogoutAlert = false
-
+    @State private var isLoggedOut = false
+    
     var body: some View {
-        NavigationView {
-            List {
-                NavigationLink(destination: SafariView(url: URL(string: "https://tagplayground.notion.site/70ca9d78786f4f529e3803c7bf59228e")!)) {
-                    Text("개인정보 정책 및 서비스 이용약관")
+        NavigationStack {
+            ZStack {
+                Color.backgroundColor
+                    .ignoresSafeArea()
+                List {
+                    NavigationLink(destination: SafariView(url: URL(string: "https://tagplayground.notion.site/70ca9d78786f4f529e3803c7bf59228e")!)) {
+                        Text("개인정보 정책 및 서비스 이용약관")
+                    }
+                    
+                    NavigationLink(destination: SafariView(url: URL(string: "https://tagplayground.notion.site/9ab7b734864a4d1bba681e3c52bde34f?v=5908c97718664315b2c31bca9f3304d8&pvs=4")!)) {
+                        Text("개발자 정보")
+                    }
+                    
+                    Button(action: {
+                        showLogoutAlert = true
+                    }) {
+                        Text("로그아웃")
+                            .foregroundColor(.red)
+                    }
+                    .alert(isPresented: $showLogoutAlert) {
+                        Alert(
+                            title: Text("로그아웃"),
+                            message: Text("로그아웃 하시겠어요?"),
+                            primaryButton: .destructive(Text("확인"), action: {
+                                signViewModel.signOut()
+                                isLoggedOut = true
+                            }),
+                            secondaryButton: .cancel(Text("취소"))
+                        )
+                    }
+                    
+                    NavigationLink(destination: Text("탈퇴하기")) {
+                        Text("탈퇴하기")
+                    }
                 }
+                .scrollContentBackground(.hidden)
+                .background(Color.clear)
                 
-                NavigationLink(destination: SafariView(url: URL(string: "https://tagplayground.notion.site/9ab7b734864a4d1bba681e3c52bde34f?v=5908c97718664315b2c31bca9f3304d8&pvs=4")!)) {
-                    Text("개발자 정보")
+                NavigationLink(destination: SignInView().environmentObject(signViewModel), isActive: $isLoggedOut) {
+                    EmptyView()
                 }
-                
-                Button(action: {
-                    showLogoutAlert = true
-                }) {
-                    Text("로그아웃")
-                        .foregroundColor(.red)
-                }
-                .alert(isPresented: $showLogoutAlert) {
-                    Alert(
-                        title: Text("로그아웃"),
-                        message: Text("로그아웃 하시겠어요?"),
-                        primaryButton: .destructive(Text("확인"), action: {
-                            signViewModel.signOut()
-                        }),
-                        secondaryButton: .cancel(Text("취소"))
-                    )
-                }
-                
-                NavigationLink(destination: Text("탈퇴하기")) {
-                    Text("탈퇴하기")
-                }
+                .hidden()
             }
             .navigationTitle("설정")
         }
@@ -60,5 +73,5 @@ struct SettingView: View {
 }
 
 #Preview {
-    SettingView()
+    SettingView().environmentObject(SignViewModel())
 }
