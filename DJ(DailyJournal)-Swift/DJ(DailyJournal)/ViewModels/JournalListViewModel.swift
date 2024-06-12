7bf62d062abee98f169876272cd8fa8cda732b2c
Eujeen Han <eujeenhan@gmail.com>
2024-06-12 11:56:32 +0900

    JournalListViewModel.swift fetchSummary(_ id:) 추가

--- DJ(DailyJournal)-Swift/DJ(DailyJournal)/ViewModels/JournalListViewModel.swift
+++ DJ(DailyJournal)-Swift/DJ(DailyJournal)/ViewModels/JournalListViewModel.swift
@@ -11,6 +11,7 @@ import UIKit
 
 class JournalListViewModel: ObservableObject {
     @Published var journals: [Journal] = []
+    @Published var summary = ""
     @Published var albumImageViewModel = AlbumImageViewModel()
     @Published var todoListViewModel = TemplateViewModel<TodoTemplateModel>()
     @Published var dailyLogViewModel = TemplateViewModel<DailyTemplateModel>()
@@ -21,10 +22,10 @@ class JournalListViewModel: ObservableObject {
         JournalService.shared.fetchJournals()
             .sink { completion in
                 switch completion {
-                    case .finished:
-                        break
-                    case .failure(let error):
-                        print(error.localizedDescription)
+                case .finished:
+                    break
+                case .failure(let error):
+                    print(error.localizedDescription)
                 }
             } receiveValue: { documents in
                 self.journals = documents
@@ -36,14 +37,29 @@ class JournalListViewModel: ObservableObject {
             }.store(in: &cancellables)
     }
     
+    func fetchSummary(_ id: Int) {
+        JournalService.shared.fetchSummary(id)
+            .sink { completion in
+                switch completion {
+                case .finished:
+                    break
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+            } receiveValue: { summary in
+                self.summary = summary.summary
+                print(self.summary)
+            }.store(in: &cancellables)
+    }
+    
     func updateJournal(_ journal: UpdatedJournal) {
         JournalService.shared.updateJournal(journal)
             .sink { completion in
                 switch completion {
-                    case .finished:
-                        print("Journal 업데이트 성공")
-                    case .failure(let error):
-                        print("Journal 업데이트 실패\(error.localizedDescription)")
+                case .finished:
+                    print("Journal 업데이트 성공")
+                case .failure(let error):
+                    print("Journal 업데이트 실패\(error.localizedDescription)")
                 }
             } receiveValue: { updatedJournal in
                 if let index = self.journals.firstIndex(where: { $0.id == updatedJournal.id }) {
